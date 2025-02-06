cd908b6591ff74da715c93cba463e4f41fea0d9f
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-02-06 19:52:59 +0100

    navbar UI fixes and playlist schema create

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import {auth, signIn, signOut} from "@/auth";
+import { BadgePlus, LogIn, LogOut } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 const Navbar = async () => {
     const session = await auth()
@@ -16,18 +18,25 @@ const Navbar = async () => {
                     {session && session?.user ? (
                         <>
                             <Link href={'/startup/create'}>
-                                <span>Create</span>
+                                <span className="max-sm:hidden">Create</span>
+                                <BadgePlus className="size-6 sm:hidden"/>
                             </Link>
 
                             <form action={async () => {
                                 "use server"
                                 await signOut({redirectTo: "/"});
                             }}>
-                                <button type={"submit"}>Logout</button>
+                                <button type={"submit"}>
+                                    <span className="max-sm:hidden">Logout</span>
+                                    <LogOut className="size-6 mt-1.5 sm:hidden text-red-500"/>
+                                </button>
                             </form>
 
                             <Link href={`/user/${session?.id}`}>
-                                <span>{session?.user?.name}</span>
+                                <Avatar className="size-10">
+                                    <AvatarImage src={session?.user?.image || ""} alt={session?.user?.name || ""}/>
+                                    <AvatarFallback>AV</AvatarFallback>
+                                </Avatar>
                             </Link>
                         </>
                     ): (
@@ -36,7 +45,10 @@ const Navbar = async () => {
                                 "use server"
                                 await signIn('github')
                             }}>
-                                <button type={"submit"}>Login</button>
+                                <button type={"submit"}>
+                                    <span className="max-sm:hidden">Log In</span>
+                                    <LogIn className="sm:hidden"/>
+                                </button>
                             </form>
                         </>
                     )}
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -6,5 +6,6 @@ export const structure: StructureResolver = (S) =>
     .title('Content')
     .items([
         S.documentTypeListItem("author").title("Authors"),
-        S.documentTypeListItem("startup").title("Startup")
+        S.documentTypeListItem("startup").title("Startup"),
+        S.documentTypeListItem("playlist").title("Playlist")
     ]);
